31785c2f02309dc1123683400c42bbb9f45b47f8
Sara Cemal <saracemal@gmail.com>
2020-10-20 19:44:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |element|
-    puts "Hello {"
+    puts "Hello {element}!"
 end
 
 def list_dwarves(array)
