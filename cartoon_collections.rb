d30d09edc7ba933239117bf57f46a9fb9397a464
Sara Cemal <saracemal@gmail.com>
2020-10-20 19:56:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,6 +4,6 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index do |element, index|
+  array.each_with_index do |index, element|
       puts "#{index}. #{element}"
 end
